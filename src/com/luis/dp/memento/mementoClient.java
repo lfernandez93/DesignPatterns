ad1f732799287b60745593dcaa375d9953b3f4de
Luis Fernandez <luisergio93@hotmail.com>
2015-02-26 15:04:15 -0600

    doing memento not yet implemented

--- src/com/luis/dp/memento/mementoClient.java
+++ src/com/luis/dp/memento/mementoClient.java
@@ -7,8 +7,11 @@
 package com.luis.dp.memento;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -21,25 +24,13 @@ import javafx.stage.Stage;
 public class mementoClient extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Scene scene = new Scene(root);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
@@ -48,5 +39,4 @@ public class mementoClient extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
