460c96d72fd4e3031fcbee4e278c0ef22c94b9d2
Luis Fernandez <luisergio93@hotmail.com>
2015-02-26 14:41:19 -0600

    doing memento not yet implemented

---
+++ src/com/luis/dp/memento/Client.java
@@ -0,0 +1,63 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luis.dp.memento;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author 984201
+ */
+public class Client {
+
+    private String firstName;
+    private String lastName;
+    private char sex;
+    private Date dob;
+    private int id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
